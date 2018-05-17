c421fe3db7b3903d75497279fc5f9e5e9f492110
0Calories <cppanand@gmail.com>
2018-05-16 20:31:03 -0400

    Initial commit

---
+++ server/callbell-server.js
@@ -0,0 +1,25 @@
+const notifier = require('node-notifier');
+const express = require('express');
+
+const port = 4200;
+const app = express();
+
+
+// POST Request to react when call bell activated
+app.post('/call', (req, res) => {
+
+    notifier.notify({
+        title: 'Call Bell',
+        message: 'You have been summoned!',
+        icon: './../resources/bell-icon.png',
+        wait: false
+    });
+
+    res.status(200).send();
+});
+
+// Start up the server and listen for incoming connections
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
+
